37a91b6020ee246c09fabe984b6d1d018be9cd91
Mario Rubio <mario.dagoberto.rubio@gmail.com>
2021-04-28 14:15:33 -0600

    WIP

--- SISTEMA DE ADMISION/RETORNO_EXPEDIENTE.Designer.vb
+++ SISTEMA DE ADMISION/RETORNO_EXPEDIENTE.Designer.vb
@@ -46,20 +46,18 @@ Partial Class RETORNO_EXPEDIENTE
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(187, 60)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(140, 49)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(625, 31)
+        Me.Label3.Size = New System.Drawing.Size(481, 25)
         Me.Label3.TabIndex = 28
         Me.Label3.Text = " FORMULARIO PARA RETORNAR EXPEDIENTE"
         '
         'PictureBox4
         '
         Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
-        Me.PictureBox4.Location = New System.Drawing.Point(820, 13)
-        Me.PictureBox4.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox4.Location = New System.Drawing.Point(615, 11)
         Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(108, 114)
+        Me.PictureBox4.Size = New System.Drawing.Size(81, 93)
         Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox4.TabIndex = 30
         Me.PictureBox4.TabStop = False
@@ -67,29 +65,28 @@ Partial Class RETORNO_EXPEDIENTE
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(-1, 23)
-        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox2.Location = New System.Drawing.Point(-1, 19)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(180, 79)
+        Me.PictureBox2.Size = New System.Drawing.Size(135, 64)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 29
         Me.PictureBox2.TabStop = False
         '
         'TXTCOMENTARIO
         '
-        Me.TXTCOMENTARIO.Location = New System.Drawing.Point(209, 351)
+        Me.TXTCOMENTARIO.Location = New System.Drawing.Point(157, 285)
+        Me.TXTCOMENTARIO.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TXTCOMENTARIO.Name = "TXTCOMENTARIO"
-        Me.TXTCOMENTARIO.Size = New System.Drawing.Size(300, 22)
+        Me.TXTCOMENTARIO.Size = New System.Drawing.Size(226, 20)
         Me.TXTCOMENTARIO.TabIndex = 31
         '
         'Label11
         '
         Me.Label11.AutoSize = True
         Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label11.Location = New System.Drawing.Point(205, 132)
-        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Location = New System.Drawing.Point(154, 107)
         Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(144, 20)
+        Me.Label11.Size = New System.Drawing.Size(124, 17)
         Me.Label11.TabIndex = 41
         Me.Label11.Text = "Tipo de Busqueda"
         Me.Label11.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -99,28 +96,25 @@ Partial Class RETORNO_EXPEDIENTE
         Me.CMBBUSQUEDA.Enabled = False
         Me.CMBBUSQUEDA.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CMBBUSQUEDA.FormattingEnabled = True
-        Me.CMBBUSQUEDA.Location = New System.Drawing.Point(209, 156)
-        Me.CMBBUSQUEDA.Margin = New System.Windows.Forms.Padding(4)
+        Me.CMBBUSQUEDA.Location = New System.Drawing.Point(157, 127)
         Me.CMBBUSQUEDA.Name = "CMBBUSQUEDA"
-        Me.CMBBUSQUEDA.Size = New System.Drawing.Size(300, 25)
+        Me.CMBBUSQUEDA.Size = New System.Drawing.Size(226, 21)
         Me.CMBBUSQUEDA.TabIndex = 40
         '
         'TXTEXPEDIENTE
         '
-        Me.TXTEXPEDIENTE.Location = New System.Drawing.Point(209, 218)
-        Me.TXTEXPEDIENTE.Margin = New System.Windows.Forms.Padding(4)
+        Me.TXTEXPEDIENTE.Location = New System.Drawing.Point(157, 177)
         Me.TXTEXPEDIENTE.Name = "TXTEXPEDIENTE"
-        Me.TXTEXPEDIENTE.Size = New System.Drawing.Size(300, 22)
+        Me.TXTEXPEDIENTE.Size = New System.Drawing.Size(226, 20)
         Me.TXTEXPEDIENTE.TabIndex = 39
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label8.Location = New System.Drawing.Point(205, 193)
-        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Location = New System.Drawing.Point(154, 157)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(121, 20)
+        Me.Label8.Size = New System.Drawing.Size(104, 17)
         Me.Label8.TabIndex = 38
         Me.Label8.Text = "No. Expediente"
         Me.Label8.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -135,10 +129,9 @@ Partial Class RETORNO_EXPEDIENTE
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label1.Location = New System.Drawing.Point(205, 328)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(154, 266)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(95, 20)
+        Me.Label1.Size = New System.Drawing.Size(80, 17)
         Me.Label1.TabIndex = 43
         Me.Label1.Text = "Comentario"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
@@ -149,49 +142,45 @@ Partial Class RETORNO_EXPEDIENTE
         Me.BTNBUSCAR.BackgroundImage = CType(resources.GetObject("BTNBUSCAR.BackgroundImage"), System.Drawing.Image)
         Me.BTNBUSCAR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.BTNBUSCAR.FlatAppearance.BorderSize = 50
-        Me.BTNBUSCAR.Location = New System.Drawing.Point(517, 214)
-        Me.BTNBUSCAR.Margin = New System.Windows.Forms.Padding(4)
+        Me.BTNBUSCAR.Location = New System.Drawing.Point(388, 174)
         Me.BTNBUSCAR.Name = "BTNBUSCAR"
-        Me.BTNBUSCAR.Size = New System.Drawing.Size(41, 31)
+        Me.BTNBUSCAR.Size = New System.Drawing.Size(31, 25)
         Me.BTNBUSCAR.TabIndex = 44
         Me.BTNBUSCAR.UseVisualStyleBackColor = False
         '
         'TXTPACIENTE
         '
-        Me.TXTPACIENTE.Location = New System.Drawing.Point(209, 286)
-        Me.TXTPACIENTE.Margin = New System.Windows.Forms.Padding(4)
+        Me.TXTPACIENTE.Location = New System.Drawing.Point(157, 232)
         Me.TXTPACIENTE.Name = "TXTPACIENTE"
-        Me.TXTPACIENTE.Size = New System.Drawing.Size(300, 22)
+        Me.TXTPACIENTE.Size = New System.Drawing.Size(226, 20)
         Me.TXTPACIENTE.TabIndex = 46
         '
         'Label9
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Me.Label9.Location = New System.Drawing.Point(205, 261)
-        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Location = New System.Drawing.Point(154, 212)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(165, 20)
+        Me.Label9.Size = New System.Drawing.Size(140, 17)
         Me.Label9.TabIndex = 45
         Me.Label9.Text = "Nombre del Paciente"
         Me.Label9.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'BTNCONFIRMAR
         '
-        Me.BTNCONFIRMAR.Location = New System.Drawing.Point(209, 401)
-        Me.BTNCONFIRMAR.Margin = New System.Windows.Forms.Padding(4)
+        Me.BTNCONFIRMAR.Location = New System.Drawing.Point(157, 326)
         Me.BTNCONFIRMAR.Name = "BTNCONFIRMAR"
-        Me.BTNCONFIRMAR.Size = New System.Drawing.Size(200, 39)
+        Me.BTNCONFIRMAR.Size = New System.Drawing.Size(150, 32)
         Me.BTNCONFIRMAR.TabIndex = 47
         Me.BTNCONFIRMAR.Text = "Confirmar Entrega"
         Me.BTNCONFIRMAR.UseVisualStyleBackColor = True
         '
         'RETORNO_EXPEDIENTE
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Gray
-        Me.ClientSize = New System.Drawing.Size(931, 497)
+        Me.ClientSize = New System.Drawing.Size(698, 404)
         Me.Controls.Add(Me.BTNCONFIRMAR)
         Me.Controls.Add(Me.TXTPACIENTE)
         Me.Controls.Add(Me.Label9)
@@ -205,7 +194,9 @@ Partial Class RETORNO_EXPEDIENTE
         Me.Controls.Add(Me.PictureBox4)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Label3)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "RETORNO_EXPEDIENTE"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "RETORNO_EXPEDIENTE"
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
